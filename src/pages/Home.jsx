177f2e3944ea9139bede86d00b7bb5218facd59f
mystixxx <okejhere@gmail.com>
2023-09-05 02:20:50 +0200

    routes, paging

---
+++ src/pages/Home.jsx
@@ -0,0 +1,23 @@
+ import React from 'react'
+ import Hero from "../components/Hero/Hero";
+ import Models from "../components/Models/Models";
+ import Calculator from "../components/Calculator/Calculator";
+ import Features from "../components/Features/Features";
+ import Testimonials from "../components/Testimonials/Testimonials";
+ import BlogLanding from "../components/BlogLanding/BlogLanding";
+ 
+ const Home = () => {
+   return (
+    <>
+      <Hero />
+      <Models />
+      <Calculator />
+      <Features />
+      <Testimonials />
+      <BlogLanding />
+    </>
+   )
+ }
+ 
+ export default Home
+ 
