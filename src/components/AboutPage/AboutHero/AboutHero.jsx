46622d182f3e35a6ce8f23c73f28a48d6a84a0e1
mystixxx <okejhere@gmail.com>
2023-09-07 22:18:42 +0200

    about page

--- src/components/AboutPage/AboutHero/AboutHero.jsx
+++ src/components/AboutPage/AboutHero/AboutHero.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./AboutHero.scss";
+import browserTop from "../../../assets/lighttopbar.webp";
+import browserbg from "../../../assets/browserbackground.webp";
 
 const AboutHero = () => {
   return (
@@ -9,6 +11,20 @@ const AboutHero = () => {
         <h1 className="title">
           Modestiam vel temperantiam, quae est moderatio
         </h1>
+        <div className="abouthero_container_image">
+          <img src={browserTop} alt="browser top" />
+          <img src={browserbg} alt="browser bg" />
+        </div>
+        <h2 className="h2Regular secondtitle">
+          Transfer idem ad modestiam vel temperantiam, quae est moderatio
+          cupiditatum rationi oboediens
+        </h2>
+        <p className="subheading bottomtext">
+          Quod, inquit, quamquam voluptatibus quibusdam est saepe iucundius,
+          tamen expetitur propter voluptatem. De illis, cum volemus. Sed nimis
+          multa. Mihi enim satis est, ipsis non satis. Pauca mutat vel plura
+          sane
+        </p>
       </div>
     </div>
   );
