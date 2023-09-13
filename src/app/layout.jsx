6ff4998b877e54eaac1d13e750062b2dec57b069
Frane Lukin <frane@Franes-MacBook-Pro.local>
2023-09-13 21:11:05 +0200

    Load svg icons

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -11,6 +11,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
+      <head>
+        <meta charSet='utf-8' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <link
+          rel='stylesheet'
+          href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css'
+        />
+        <title>Botik</title>
+      </head>
       <body>
         <Navbar />
         {children}
