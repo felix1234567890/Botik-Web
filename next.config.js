c01d5b9ff5f2384f0b3438813358877ff2fed495
Frane Lukin <frane@Franes-MacBook-Pro.local>
2023-09-13 20:25:29 +0200

    Specified build directory

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: { domains: ["images.unsplash.com"] }
+  images: { domains: ["images.unsplash.com"] },
+  distDir: 'build',
 }
 
 module.exports = nextConfig
